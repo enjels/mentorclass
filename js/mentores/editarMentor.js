7bf9e353dfde2e48fd8a239a9fc314bebbd51953
Virgílio César <90569314+enjels@users.noreply.github.com>
2023-07-07 01:05:59 -0300

    paginas de mentores 100% completas  e funcionando

--- js/mentores/editarMentor.js
+++ js/mentores/editarMentor.js
@@ -1,24 +1,54 @@
+const  formulario = document.getElementById('formulario')
 let mentoresId = null
 
 //recupera o ID do mentor que foi enviado no parametro da URL
 const getIdUrl = () => {
-    const parameString = window.location.search
+    console.log(window.location.search)
+    const paramString = window.location.search
     // URLSearchParams define metodos para buscar parametros da URL
-    const parameters = URLSearchParams(parameString)
+    const pararms = new URLSearchParams(paramString)
     // params.get('o_nome_do_campo_que_esta_na_url)'
     // ou seja, ao utulizar o .get eu posso recuperar um determinado campo
-    mentoresId = parameters.get('Id')
+    mentoresId = pararms.get('id')
 }
+
 
 // recuperar mentor
 const buscarMentores = async () => {
-    const retorno = await fetch(`http://localhost:3000/mentores/${mentoresId}`)
-    const mentores = retorno.json()
+    // Dou um fetch na url do json-server enviando o id dos mentores
+    const response= await fetch(`http://localhost:3000/mentores/${mentoresId}`)
+    const mentores = await response.json()
     return mentores
 }
 
-const editarMentores = async (mentores) => {
+// Envia os dados para o api jason server
 
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const nome = formulario.elements['nome'].value
+    const email = formulario.elements['email'].value
+
+    const mentores = {
+        nome,
+        email,
+
+    }
+   editarMentores(mentores)
+
+
+
+})
+
+// Injeto os dados do livro no formulario, com dados do json
+const carregarDadosFormulario = async (mentores) => {
+    document.getElementById('nome').value = mentores.nome
+    document.getElementById('email').value = mentores.email
+
+
+
+}
+
+const editarMentores = async (mentores) => {
     await fetch(`http://localhost:3000/mentores/${mentoresId}`, {
         method: 'PUT',
         headers: {
@@ -26,34 +56,11 @@ const editarMentores = async (mentores) => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(mentores)
-
     })
     // Redireciono o usuario para a tela de listagem
     window.location = "/html/mentores/mentores.html"
 
 }
-
-const carregarDadosFormulario = async (mentores) => {
-    document.getElementById('nome').value = mentores.nome
-    document.getElementById('email').value = mentores.email
-
-}
-
-// Envia os dados para o api jason server
-const  formulario = document.getElementById('formulario')
-formulario.addEventListener('submit', (event) => {
-    event.preventDefault()
-    const nome = document.getElementById('nome').value
-    const email = document.getElementById('email').value
-
-    const mentores = {
-        nome,
-        email
-
-    }
-    editarMentores(mentores)
-
-})
 
 // Ao entrar na página, a funcão carregarDados é chamada
 const carregarDados = async () => {
