c320c82c74b71aa63f118e76ea2c4fdb14ea3b87
Virgílio César <90569314+enjels@users.noreply.github.com>
2023-07-17 14:50:52 -0300

    quase pronto

--- js/turmas/editarTurma.js
+++ js/turmas/editarTurma.js
@@ -1,174 +1,174 @@
-const formulario = document.getElementById('formulario')
-let turmasId = null
+const formulario = document.getElementById('formulario');
+let turmasId = null;
+
 const buscarMentoria = async (id) => {
-    const response = await fetch(`http://localhost:3000/mentorias/${id}`)
-    const mentoria = response.json()
-    return mentoria
-}
-const buscarMentorias = async () =>{
-    const response = await fetch(`http://localhost:3000/mentorias/`)
-    const mentorias = response.json()
-    return mentorias
-}
+  const response = await fetch(`http://localhost:3000/mentorias/${id}`);
+  const mentoria = await response.json();
+  return mentoria;
+};
 
-const carregarSelectMentoria = async ()=> {
-    const mentorias = await buscarMentorias()
-    const mentoriaSelect = document.getElementById('mentoria')
+const buscarMentorias = async () => {
+  const response = await fetch(`http://localhost:3000/mentorias/`);
+  const mentorias = await response.json();
+  return mentorias;
+};
 
-    const opcaoVazia = new Option('Selecione uma mentoria...')
-    mentoriaSelect.options.add(opcaoVazia)
+const carregarSelectMentoria = async () => {
+  const mentorias = await buscarMentorias();
+  const mentoriaSelect = document.getElementById('mentoria');
 
-    mentorias.forEach(mentorias => {
-    const opcao = new Option(mentorias.mentoria, mentorias.id)
-    mentoriaSelect.options.add(opcao)
-    });
-}
+  const opcaoVazia = new Option('Selecione uma mentoria...');
+  mentoriaSelect.options.add(opcaoVazia);
 
+  mentorias.forEach((mentoria) => {
+    const opcao = new Option(mentoria.mentoria, mentoria.id);
+    mentoriaSelect.options.add(opcao);
+  });
+};
 
-const buscarMentor = async (id)=> {
-    const resposta = await fetch(`http://localhost:3000/mentores/${id}`)
-    const mentor = await resposta.json()
-    return mentor
-}
+const buscarMentor = async (id) => {
+  const resposta = await fetch(`http://localhost:3000/mentores/${id}`);
+  const mentor = await resposta.json();
+  return mentor;
+};
 
-const buscarMentores = async ()=> {
-    const resposta = await fetch(`http://localhost:3000/mentores`)
-    const mentores = await resposta.json()
-    return mentores
-}
+const buscarMentores = async () => {
+  const resposta = await fetch(`http://localhost:3000/mentores`);
+  const mentores = await resposta.json();
+  return mentores;
+};
 
-const carregarSelectMentores = async ()=> {
-    const mentores = await buscarMentores()
-    const mentorSelect = document.getElementById('mentor')
+const carregarSelectMentores = async () => {
+  const mentores = await buscarMentores();
+  const mentorSelect = document.getElementById('mentor');
 
-    const opcaoVazia = new Option('Selecione um mentor...')
-    mentorSelect.options.add(opcaoVazia)
+  const opcaoVazia = new Option('Selecione um mentor...');
+  mentorSelect.options.add(opcaoVazia);
 
-    mentores.forEach(mentores => {
-    const opcao = new Option(mentores.mentor ,mentores.id)
-    mentorSelect.options.add(opcao)
-    });
-}
-
+  mentores.forEach((mentor) => {
+    const opcao = new Option(mentor.mentor, mentor.id);
+    mentorSelect.options.add(opcao);
+  });
+};
 
 const buscarSemana = async (id) => {
-    const response = await fetch(`http://localhost:3000/semanas/${id}`)
-    const semanas = await response.json()
-    return semanas
+  const response = await fetch(`http://localhost:3000/semanas/${id}`);
+  const semana = await response.json();
+  return semana;
+};
 
-}
 const buscarSemanas = async () => {
-    const response = await fetch('http://localhost:3000/semanas/')
-    const buscarSemanas = await response.json()
-    return buscarSemanas
-}
-const carregarSelectDiasDaSemana = async ()=> {
-    const semana = await buscarSemanas()
-    const semanaSelect = document.getElementById('dia-da-semana')
+  const response = await fetch('http://localhost:3000/semanas/');
+  const semanas = await response.json();
+  return semanas;
+};
 
-    const opcaoVazia = new Option('Selecione um dia da semana...')
-    semanaSelect.options.add(opcaoVazia)
+const carregarSelectDiasDaSemana = async () => {
+  const semanas = await buscarSemanas();
+  const semanaSelect = document.getElementById('dia-da-semana');
 
-    semana.forEach(semanas => {
-    const opcao = new Option(semanas.semana, semanas.id)
-    semanaSelect.options.add(opcao)
-    });
-}
+  const opcaoVazia = new Option('Selecione um dia da semana...');
+  semanaSelect.options.add(opcaoVazia);
 
+  semanas.forEach((semana) => {
+    const opcao = new Option(semana.semana, semana.id);
+    semanaSelect.options.add(opcao);
+  });
+};
 
 const getIdUrl = () => {
-    const paramsString = window.location.search
-    const params = new URLSearchParams(paramsString)
-    turmasId = params.get('id')
- }
+  const paramsString = window.location.search;
+  const params = new URLSearchParams(paramsString);
+  turmasId = params.get('id');
+};
 
- const buscarTurmas = async () => {
-    const resposta = await fetch(`http://localhost:3000/turmas/${turmasId}`)
-     const turmas = await resposta.json()
-     return turmas
- }
+const buscarTurmas = async () => {
+  const resposta = await fetch(`http://localhost:3000/turmas/${turmasId}`);
+  const turmas = await resposta.json();
+  return turmas;
+};
 
 const editarTurma = async (turmas) => {
-    await fetch(`http://localhost:3000/turmas/${turmasId}`,{
-        method: 'PUT',
-        headers: {
-            "Accept": 'application/json, text/plain, */*',
-            "Content-Type": 'application/json'
-        },
-        body: JSON.stringify(turmas)
-    })
-   window.location = "/html/turmas/turmas.html"
-}
+  await fetch(`http://localhost:3000/turmas/${turmasId}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json, text/plain, */*',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(turmas),
+  });
+  window.location = '/html/turmas/turmas.html';
+};
 
 const carregarDadosFormulario = async (turmas) => {
-    document.getElementById('turma').value = turmas.turma
-    document.getElementById('mentor').value = turmas.mentor
-    document.getElementById('mentoria').value = turmas.mentoria
-    document.getElementById('data-de-inicio').value = turmas.datadeinicio
-    document.getElementById('dia-da-semana').value = turmas.diadasemana
-    document.getElementById('start-time').value = turmas.horarioinicio
-    document.getElementById('end-time').value = turmas.horariofim
-    document.getElementById('encontro').value = turmas.encontros
-    document.getElementById('link-aula').value = turmas.link
-}
+  document.getElementById('turma').value = turmas.turma;
+  document.getElementById('mentor').value = turmas.mentor;
+  document.getElementById('mentoria').value = turmas.mentoria;
+  document.getElementById('data-de-inicio').value = turmas.datadeinicio;
+  document.getElementById('dia-da-semana').value = turmas.diadasemana;
+  document.getElementById('start-time').value = turmas.horarioinicio;
+  document.getElementById('end-time').value = turmas.horariofim;
+  document.getElementById('encontro').value = turmas.encontros;
+  document.getElementById('link-aula').value = turmas.link;
+};
 
-formulario.addEventListener('submit',async (event) => {
-event.preventDefault()
+formulario.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const turma = formulario.elements['turma'].value
-    const mentor = formulario.elements['mentor'].value
-    const mentoria = formulario.elements['mentoria'].value
-    const datadeinicio = formulario.elements['data-de-inicio'].value
-    const diadasemana = formulario.elements['dia-da-semana'].value
-    const horarioinicio = formulario.elements['start-time'].value
-    const horariofim = formulario.elements['end-time'].value
-    const encontros = formulario.elements['encontro'].value
-    const link = formulario.elements['link-aula'].value
+  const turma = formulario.elements['turma'].value;
+  const mentor = formulario.elements['mentor'].value;
+  const mentoria = formulario.elements['mentoria'].value;
+  const datadeinicio = formulario.elements['data-de-inicio'].value;
+  const diadasemana = formulario.elements['dia-da-semana'].value;
+  const horarioinicio = formulario.elements['start-time'].value;
+  const horariofim = formulario.elements['end-time'].value;
+  const encontros = formulario.elements['encontro'].value;
+  const link = formulario.elements['link-aula'].value;
 
-    const mentorObjeto = await buscarMentor(mentor)
-    const mentoriaObjeto = await buscarMentoria(mentoria)
-    const diadasemanaObjeto = await buscarSemana(diadasemana)
-    const turmas = {
-        turma,
-        mentor:mentorObjeto.mentor,
-        mentoria:mentoriaObjeto.mentoria,
-        datadeinicio,
-        diadasemana:diadasemanaObjeto.semana,
-        horarioinicio,
-        horariofim,
-        encontros,
-        link
-    }
-    editarTurma(turmas)
+  const mentorObjeto = await buscarMentor(mentor);
+  const mentoriaObjeto = await buscarMentoria(mentoria);
+  const diadasemanaObjeto = await buscarSemana(diadasemana);
+  const turmasObjeto = {
+    turma,
+    mentor: mentorObjeto.mentor,
+    mentoria: mentoriaObjeto.mentoria,
+    datadeinicio,
+    diadasemana: diadasemanaObjeto.semana,
+    horarioinicio,
+    horariofim,
+    encontros,
+    link,
+  };
 
-})
+  editarTurma(turmasObjeto);
+});
 
 const carregarDados = async () => {
-    getIdUrl()
+  getIdUrl();
 
-    await carregarSelectMentoria()
-    await carregarSelectMentores()
-    await carregarSelectDiasDaSemana()
+  await carregarSelectMentoria();
+  await carregarSelectMentores();
+  await carregarSelectDiasDaSemana();
 
-    const turmas = await buscarTurmas()
-    carregarDadosFormulario(turmas)
-}
-carregarDados()
+  const turmas = await buscarTurmas();
+  carregarDadosFormulario(turmas);
+};
+
+carregarDados();
 
 //funções menu/navegar
 const mentores = () => {
-    window.location = "/html/mentores/mentores.html"
-}
+  window.location = '/html/mentores/mentores.html';
+};
 //vai para pagina mentoias
 const mentorias = () => {
-    window.location = "/html/mentorias/mentorias.html"
-}
+  window.location = '/html/mentorias/mentorias.html';
+};
 //vai para pagina turmas
 const turmas = () => {
-    window.location = "/html/turmas/turmas.html"
-}
+  window.location = '/html/turmas/turmas.html';
+};
 //leva para pagina alunos
 const alunos = () => {
-    window.location = "/html/alunos/alunos.html"
-}
-
+  window.location = '/html/alunos/alunos.html';
+};
